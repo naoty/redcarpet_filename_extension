a89e9536729663aba9cc9cf2ddd4a47ccf2ad6bf
nekova <nekova07@gmail.com>
2014-04-28 22:05:30 +0900

    Fix typo

--- spec/redcarpet_filename_extension/pygments_options_spec.rb
+++ spec/redcarpet_filename_extension/pygments_options_spec.rb
@@ -3,7 +3,7 @@ require "redcarpet"
 require "pathname"
 
 describe RedcarpetFilenameExtension::BlockCodeWithFilename do
-  let(:fixtures_path) {  Pathname.new("../fixtures").expand_path(File.dirname(__FILE__)) }
+  let(:fixtures_path) { Pathname.new("../fixtures").expand_path(File.dirname(__FILE__)) }
   let(:sample_path) { fixtures_path.join("sample_with_filename.md") }
 
   context "When specified pygments lineno option" do
@@ -12,7 +12,7 @@ describe RedcarpetFilenameExtension::BlockCodeWithFilename do
       Redcarpet::Markdown.new(renderer, fenced_code_blocks: true)
     end
 
-    it "generates lineno block by paygments" do
+    it "generates lineno block by pygments" do
       html = markdown.render(sample_path.read)
       expect(html).to match /<div class="highlight">/
       expect(html).to match /<div class="linenodiv">/
@@ -25,7 +25,7 @@ describe RedcarpetFilenameExtension::BlockCodeWithFilename do
       Redcarpet::Markdown.new(renderer, fenced_code_blocks: true)
     end
 
-    it "dosen't generate lineno block by paygments" do
+    it "doesn't generate lineno block by pygments" do
       html = markdown.render(sample_path.read)
       expect(html).to match /<div class="highlight">/
       expect(html).not_to match /<div class="linenodiv">/
